6dc3118c517c2a77b3673d35c712aa5858443d13
yassir <yassir.nini27@gmail.com>
2024-05-31 03:29:56 +0100

    make it better

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const http = require('http');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port =  8080;
 
 // Create a connection to the database
 const connection = mysql.createConnection({
